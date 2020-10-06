e5635c5a3e24f310409c7830a9fd37bf374ccb7c
vortiz4 <71564102+vortiz4@users.noreply.github.com>
2020-10-06 17:12:59 -0500

    Welcome Page Edited
    - added an error message above the form
    - also didn't need a react-uuid
    - added a few string functions for the error message to show and hid
    - took out button tag in <link></link>

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -35,36 +35,14 @@ class AddNote extends Component {
 		event.preventDefault();
 	}
 
-	/* previous code from Form React, easy to understand! */
-
-	// class AddNote extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {
-	// 		value: "Jot down your thoughts here!",
-	// 	};
-
-	// 	this.handleChange = this.handleChange.bind(this);
-	// 	this.handleSubmit = this.handleSubmit.bind(this);
-	// }
-
-	// handleChange(event) {
-	// 	this.setState({ value: event.target.value });
-	// }
-
-	// handleSubmit(event) {
-	// 	alert("A note was added: " + this.state.value);
-	// 	event.preventDefault();
-	// }
-
 	render() {
 		return (
 			<div>
 		{/** This is where the Added notes appear */}
 				<div className="entries">
 					<ul>
-						{this.state.items.map((val) => (
-							<li>
+						{this.state.items.map((val, index) => (
+							<li key={index}>
 								{val.title}: {val.note}{" "}
 							</li>
 						))}
@@ -82,24 +60,9 @@ class AddNote extends Component {
 					<button className="add_note" type="submit">Add Note</button>
 				</form>
 				</div>
-
-				{/* previous code from Form React */}
-
-				{/* <form onSubmit={this.handleSubmit}>
-					<label className="your_note">
-						Your Note:
-						<textarea className="container" value={this.state.value} onChange={this.handleChange} />
-					</label>
-					<button className="button" type="submit">
-						Add
-					</button>
-				</form> */}
 			</div>
 		);
 	}
 }
-
-//** react-uuid library is still installed in this project now I need help with the documenting the uuids since each note needs a unique "key" according to the chrome debugger console */
-//** This was easier to understand */
 
 export default AddNote;
--- src/pages/welcome-page.js
+++ src/pages/welcome-page.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../components/welcome-page.css";
 import "../components/App.css";
 
 function WelcomeScreen(props) {
+	const [errorMessage, setErrorMessage] = useState();
+
 	const { name, setName } = props;
 
 	const onNameChange = (event) => {
 		setName(event.target.value);
+		if (name !== ""){
+			setErrorMessage("")
+		}
+	};
+
+	const onNextClick = (event) => {
+		if (name === "") {
+			event.preventDefault();
+			setErrorMessage("Please enter a name")
+		}
 	};
 
 	return (
 		<main>
 			<h1 className="welcome">Welcome to JotIt!</h1>
 			<form onSubmit={(e) => e.preventDefault()}>
+				<div>{errorMessage}</div>
 				<label>
 					<input
 						type="text"
@@ -23,8 +36,8 @@ function WelcomeScreen(props) {
 					/>
 				</label>
 			</form>
-			<Link to="/entry" className="next_page">
-				<button className="button">Next</button>
+			<Link to="/entry" className="button" onClick={onNextClick}>
+				Next
 			</Link>
 		</main>
 	);
